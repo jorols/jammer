5b645692c070cfa3e5b958715904eff50f920b96
jorols <119048789+jorols@users.noreply.github.com>
2024-09-25 09:49:32 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
-input.onButtonPressed(Button.A, function () {
-    sender_id += -1
-    if (sender_id < 0) {
-        sender_id = 5
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    sender_id += 1
-    if (sender_id > 5) {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 6) {
         sender_id = 0
+    } else if (receivedNumber == 7) {
+        sender_id = 1
+    } else if (receivedNumber == 8) {
+        sender_id = 2
+    } else if (receivedNumber == 9) {
+        sender_id = 3
+    } else if (receivedNumber == 10) {
+        sender_id = 4
+    } else if (receivedNumber == 11) {
+        sender_id = 5
     }
 })
 let sender_id = 0
